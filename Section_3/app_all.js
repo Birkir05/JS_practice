3fb9de7661e21f071f5e86b7a933d44521bace7e
Birkir Snaer Axelsson <birkir.snaer@icloud.com>
2024-07-30 23:01:39 +0000

    Section_3

---
+++ Section_3/app_all.js
@@ -0,0 +1,14 @@
+console.log(3<2<1)
+
+function greet(name) {
+    name = name || '<Your name here>';
+    console.log('Hello ' + name);
+}
+
+
+greet('Birkir')
+greet()
+
+//______________________
+
+console.log(libraryName);
